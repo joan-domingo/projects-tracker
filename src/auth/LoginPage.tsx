3bb228be145c62de09d671020f5d80bfcf7b4409
Joan Domingo <joands@gmail.com>
2019-10-12 19:11:05 +0200

    beautify login page a bit

--- src/auth/LoginPage.tsx
+++ src/auth/LoginPage.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import i18n from '../i18n/i18n';
 import Button from '../shared/components/Button';
 import { loginAction } from './auth.redux';
 
+const LoginPageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
 const LoginPage: FC = () => {
   const dispatch = useDispatch();
   return (
-    <div>
+    <LoginPageContainer>
       <Button
         onClick={handleOnClickLogin}
         label={i18n.t('loginPage.loginButton')}
         type="primary"
       />
-    </div>
+    </LoginPageContainer>
   );
 
   function handleOnClickLogin() {
