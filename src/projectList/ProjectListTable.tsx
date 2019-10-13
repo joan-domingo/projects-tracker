adeb5a728195de3ca42a12dd02501632fce5a4ac
Joan Domingo <joands@gmail.com>
2019-10-13 10:04:37 +0200

    localize project list translations and polish it a bit

--- src/projectList/ProjectListTable.tsx
+++ src/projectList/ProjectListTable.tsx
@@ -1,8 +1,7 @@
 import AddIcon from '@material-ui/icons/AddCircle';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
 import moment from 'moment';
 import React, { FC } from 'react';
+import i18n from '../i18n/i18n';
 import Table from '../shared/components/Table';
 import { ProjectUpdate } from '../shared/models/ProjectData';
 
@@ -14,10 +13,12 @@ interface Props {
 const ProjectListTable: FC<Props> = props => {
   return (
     <Table
-      title="Projects"
+      title={i18n.t('projectList.table.title')}
       columns={columns}
       data={props.data}
       actions={getTableActions(props)}
+      // tslint:disable-next-line: no-empty
+      onRowClick={() => {}}
     />
   );
 };
@@ -26,23 +27,9 @@ const getTableActions = (props: Props) => {
   return [
     {
       icon: () => <AddIcon />,
-      tooltip: 'Add Project',
+      tooltip: i18n.t('projectList.addProject'),
       onClick: props.onAddProject,
       isFreeAction: true,
-    },
-    {
-      icon: () => <EditIcon />,
-      tooltip: 'Edit Project',
-      onClick: () => {
-        alert('Edit Project');
-      },
-    },
-    {
-      icon: () => <DeleteIcon />,
-      tooltip: 'Delete Project',
-      onClick: () => {
-        alert('Delete Project');
-      },
     },
   ];
 };
@@ -50,10 +37,10 @@ const getTableActions = (props: Props) => {
 const columns = [
   {
     field: 'timeMillis',
-    title: 'Last Update',
-    render: (timeMillis: number) => <p>{moment(timeMillis).fromNow()}</p>,
+    title: i18n.t('projectList.table.lastUpdate'),
+    render: (timeMillis: number) => moment(timeMillis).fromNow(),
   },
-  { field: 'projectName', title: 'Project Name' },
+  { field: 'projectName', title: i18n.t('projectList.table.projectName') },
 ];
 
 export default ProjectListTable;
